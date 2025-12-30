40615d1a7c7451229dfaba9f24487bdb936bd15e
kelvin tony <kelvintonyph@gmail.com>
2025-12-30 17:37:00 +0100

    final install

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,7 @@
 import type { Metadata } from 'next';
-import { Poppins, Lavishly_Yours } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-
-const poppins = Poppins({
-  variable: '--font-poppins',
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-});
-
-const lavishlyYours = Lavishly_Yours({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-lavishly-yours',
-});
 
 export const metadata: Metadata = {
   title: 'CLinton Ikechukwu',
